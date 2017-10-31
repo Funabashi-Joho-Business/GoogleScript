faa7b73c09c7126da169f792605ba13f7cd06abd
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-10-31 10:49:29 +0900

    認証手順の変更

--- app/src/main/java/jp/ac/chiba_fjb/example/googlescript/GoogleAccount.java
+++ app/src/main/java/jp/ac/chiba_fjb/example/googlescript/GoogleAccount.java
@@ -1,5 +1,6 @@
 package jp.ac.chiba_fjb.example.googlescript;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Context;
@@ -7,7 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAuthIOException;
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.util.ExponentialBackOff;
@@ -20,8 +20,8 @@ import java.util.Arrays;
  */
 
 public class GoogleAccount {
-    private static final int REQUEST_ACCOUNT_PICKER = 1234;
-    private static final int REQUEST_AUTHORIZATION = 1235;
+    private static final int REQUEST_ACCOUNT_PICKER = 998;
+    private static final int REQUEST_AUTHORIZATION = 999;
     private static final String EXTRA_NAME = "SCRIPT_INFO";
     private static final String PREF_ACCOUNT_NAME = "ScriptUser";
 
@@ -38,8 +38,9 @@ public class GoogleAccount {
                 con, Arrays.asList(scope==null?SCOPES:scope))
                 .setBackOff(new ExponentialBackOff());
         //登録済みアカウント名を取得
-        mAccountName = mContext.getSharedPreferences("GOOGLE",Context.MODE_PRIVATE).getString(PREF_ACCOUNT_NAME, null);
-        mCredential.setSelectedAccountName(mAccountName);
+        mAccountName = mContext.getSharedPreferences("GOOGLE", Context.MODE_PRIVATE).getString(PREF_ACCOUNT_NAME, null);
+        Account account = new Account(mAccountName,"com.google");
+        mCredential.setSelectedAccount(account);
     }
     public GoogleAccountCredential getCredential(){
         return mCredential;
@@ -50,15 +51,17 @@ public class GoogleAccount {
     public void resetAccount(){
         //登録アカウントの解除
         SharedPreferences settings =
-                mContext.getSharedPreferences("GOOGLE",Context.MODE_PRIVATE);
+                mContext.getSharedPreferences("GOOGLE", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString(PREF_ACCOUNT_NAME, null);
         editor.apply();
         mAccountName = null;
+        //mCredential.setSelectedAccountName(null);
+        mCredential.setSelectedAccount(null);
     }
     public void requestAccount(){
         //ユーザ選択
-        if(mAccountName==null && mContext instanceof Activity)
+        if(mAccountName==null&& mContext instanceof Activity)
             ((Activity)mContext).startActivityForResult(mCredential.newChooseAccountIntent(),REQUEST_ACCOUNT_PICKER);
         else
             call();
@@ -69,10 +72,11 @@ public class GoogleAccount {
                 mAccountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
 
                 if (mAccountName != null) {
+                    Account account = new Account(mAccountName,"com.google");
                     //アカウント選択確定
-                    mCredential.setSelectedAccountName(mAccountName);
+                    mCredential.setSelectedAccount(account);
                     SharedPreferences settings =
-                            mContext.getSharedPreferences("GOOGLE",Context.MODE_PRIVATE);
+                            mContext.getSharedPreferences("GOOGLE", Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = settings.edit();
                     editor.putString(PREF_ACCOUNT_NAME, mAccountName);
                     editor.apply();
@@ -94,6 +98,27 @@ public class GoogleAccount {
                 onError();	//実行不能時の処理
         }
     }
+    protected void exception(Exception e){
+        if (e instanceof UserRecoverableAuthIOException) {
+            //権限要求の呼び出し
+            if(mContext instanceof Activity) {
+                Intent intent = ((UserRecoverableAuthIOException) e).getIntent();
+                ((Activity) mContext).startActivityForResult(intent, REQUEST_AUTHORIZATION);
+            }
+
+        } else if (e instanceof IllegalArgumentException) {
+            //アカウント要求
+            requestAccount();
+        } else if (e instanceof GoogleJsonResponseException) {
+            onError();
+            System.err.println(((GoogleJsonResponseException)e).getMessage());
+
+        } else {
+            //登録系エラー
+            onError();
+            e.printStackTrace();
+        }
+    }
     public void call(){
         new Thread() {
             @Override
@@ -102,27 +127,7 @@ public class GoogleAccount {
                     onExec();
                 }
                 catch (Exception e) {
-                    if (e instanceof UserRecoverableAuthIOException) {
-                        //権限要求の呼び出し
-                        if(mContext instanceof Activity) {
-                            Intent intent = ((UserRecoverableAuthIOException) e).getIntent();
-                            ((Activity) mContext).startActivityForResult(intent, REQUEST_AUTHORIZATION);
-                        }
-
-                    } else if (e instanceof IllegalArgumentException) {
-                        //アカウント要求
-                        requestAccount();
-                    } else if (e instanceof GoogleJsonResponseException) {
-                        onError();
-                        e.printStackTrace();
-
-                    }else if (e instanceof GoogleAuthIOException) {
-                        //登録系エラー
-                        onError();
-                        e.printStackTrace();
-                    }else {
-
-                    }
+                    exception(e);
                 }
             }
         }.start();
